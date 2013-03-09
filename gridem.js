72bf3ddbebbe8ecd5b6032026ad07ae04b11dad8
Yves Dagenais <yves@streamwave.com>
2013-03-09 16:03:03 -0500

    Changed JS variable naming convention

--- gridem.js
+++ gridem.js
@@ -7,13 +7,14 @@
 (function( $ ) {
 
   var methods = {
-    init : function (options) {
+    init : function ( options ) {
       gridItems(this, options);
+      return this;
     }
   };
 
 
-  $.fn.gridEm = function(method) {
+  $.fn.gridEm = function( method ) {
     if (methods[method]) {
       return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
     } else if ( typeof method === 'object' || !method ){
@@ -29,72 +30,73 @@
    *
    * Main function that figures out how to lay out the grid item.
    */
-  function gridItems(grid, options) {
-    if (!grid.children().length) return;
+  function gridItems( $grid, options ) {
+    if (!$grid.children().length) return;
 
     var settings = $.extend( {
-      'min_margin'     :  10,
-      'max_margin'     : 100,
+      'minMargin'     :  10,
+      'maxMargin'     : 100
     }, options);
 
-    var inline_quirk = 0;
-    var item_margin  = 0;
-    var grid_width   = grid.innerWidth();
-    var free_space   = 0;
-    var rounding_adj = 0;
+    var inlineQuirk = 0
+      , itemMargin  = 0
+      , gridWidth   = $grid.innerWidth()
+      , freeSpace   = 0
+      , roundingAdj = 0;
 
     // Assuming all items are of equal width
-    var first_item  = grid.children().first();
-    var item_width  = first_item.outerWidth();
+    var $firstItem  = $grid.children().first()
+      , itemWidth   = $firstItem.outerWidth();
     
-    if ('inline-block' == first_item.css('display')) inline_quirk = 4;
+    if ('inline-block' == $firstItem.css('display')) inlineQuirk = 4;
 
     // Calculate max items per row
-    var items_per_row = Math.floor(grid_width / item_width);
-    var item_count    = grid.children().length;
-    if (items_per_row > item_count) items_per_row = item_count;
+    var itemsPerRow = Math.floor(gridWidth / itemWidth)
+      , itemCount   = $grid.children().length;
 
-    var all_items_width = items_per_row * item_width;
+    if (itemsPerRow > itemCount) itemsPerRow = itemCount;
 
-    if (items_per_row > 1) {
-      item_margin = Math.floor((grid_width - all_items_width) / (items_per_row - 1));
-      free_space = grid_width - all_items_width;
+    var allItemsWidth = itemsPerRow * itemWidth;
+
+    if (itemsPerRow > 1) {
+      itemMargin = Math.floor((gridWidth - allItemsWidth) / (itemsPerRow - 1));
+      freeSpace = gridWidth - allItemsWidth;
     } else {
-      applyNewMargin(grid, { item_margin: settings.min_margin });
+      applyNewMargin($grid, { itemMargin: settings.minMargin });
       return;
     }
 
     // We may need to redistribute items on rows if we're below our min margin.
-    var retry_limit=item_count;
-    while (item_margin < settings.min_margin) {
-      items_per_row--;
+    var retryLimit = itemCount;
+    while (itemMargin < settings.minMargin) {
+      itemsPerRow--;
 
-      if (items_per_row > 1) {
-        all_items_width = items_per_row * item_width;
-        item_margin = Math.floor((grid_width - all_items_width) / (items_per_row - 1));
-        free_space = grid_width - all_items_width;
+      if (itemsPerRow > 1) {
+        allItemsWidth = itemsPerRow * itemWidth;
+        itemMargin = Math.floor((gridWidth - allItemsWidth) / (itemsPerRow - 1));
+        freeSpace = gridWidth - allItemsWidth;
 
-      } else if (1 == items_per_row) {
+      } else if (1 == itemsPerRow) {
         // We might need to force some margins to ensure single column display
-        if (free_space > settings.min_margin) item_margin = settings.min_margin;
+        if (freeSpace > settings.minMargin) itemMargin = settings.minMargin;
       }
-      retry_limit--;
-      if (retry_limit<=0 || items_per_row<=1) break;
+      retryLimit--;
+      if (retryLimit <= 0 || itemsPerRow <= 1) break;
     }
 
-    if (item_margin > settings.max_margin) { item_margin = settings.max_margin; } 
+    if (itemMargin > settings.maxMargin) { itemMargin = settings.maxMargin; } 
 
     // Check to see if we're short a pixel due to rounding
-    var rnd_err  = free_space - item_margin*(items_per_row-1);
-    rounding_adj = rnd_err==1 ? rnd_err : 0;
+    var rndErr  = freeSpace - itemMargin*(itemsPerRow-1);
+    roundingAdj = rndErr==1 ? rndErr : 0;
 
-    if (inline_quirk)    item_margin -= inline_quirk;
-    if (item_margin < 0) item_margin = 0;
+    if (inlineQuirk)    itemMargin -= inlineQuirk;
+    if (itemMargin < 0) itemMargin = 0;
 
     // Adjust the grid items
-    applyNewMargin(grid, { items_per_row: items_per_row,
-                           item_margin: item_margin,
-                           rounding_adj: rounding_adj });
+    applyNewMargin($grid, { itemsPerRow: itemsPerRow,
+                            itemMargin:   itemMargin,
+                            roundingAdj:  roundingAdj });
   }
 
 
@@ -103,22 +105,22 @@
    *
    * Applies the new calculated margin to the child elements.
    */
-  function applyNewMargin(grid, options) {
-    if (!grid.children().length) return;
+  function applyNewMargin( $grid, options ) {
+    if (!$grid.children().length) return;
 
     var settings = $.extend( {
-      'items_per_row' : 1,
-      'item_margin'   : 0,
-      'rounding_adj'  : 0
+      'itemsPerRow' : 1,
+      'itemMargin'   : 0,
+      'roundingAdj'  : 0
     }, options);
 
     var i=0;
-    grid.children().each(function() { 
+    $grid.children().each(function() { 
       i++;
-      if (i < settings.items_per_row || 1 == settings.items_per_row) {
-        var new_margin = settings.item_margin;
-        if (1==i) new_margin+=settings.rounding_adj;
-        $(this).css('margin-right', new_margin + 'px');
+      if (i < settings.itemsPerRow || 1 == settings.itemsPerRow) {
+        var newMargin = settings.itemMargin;
+        if (1==i) newMargin+=settings.roundingAdj;
+        $(this).css('margin-right', newMargin + 'px');
       }else {
         $(this).css('margin-right', 0);
         i=0;
